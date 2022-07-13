761c226f5cb3a14e7df98b56d72f592ac0cf3460
Tom <tomdavid890@gmail.com>
2022-07-13 20:27:43 +1200

    mutlipe pages scrappped

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,4 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.engineeringnz.org/courses-events'
-response = requests.get(url)
-soup = BeautifulSoup(response.content,'html.parser')
 
