4d2e56114d4ca90224c3904889e166c46e680610
Tom <tomdavid890@gmail.com>
2022-10-04 23:33:43 +1300

    need to fix errors

--- load_data_RS.py
+++ load_data_RS.py
@@ -1,8 +1,7 @@
-from audioop import reverse
-from pickle import TRUE
 import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
-from Webscrapper_2 import df
 from Webscrapper_2 import title
 import random
 from scipy import sparse
@@ -10,7 +9,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pymongo
 from pymongo import DESCENDING, MongoClient
  
-id =[100,200,300,400,500,600,700,800,900]
+id =[100,200,300,400,500,600]
 user_ID = random.choices(id, k=len(title))
 
 ratings = np.random.randint(1,5,size=(len(user_ID),len(title)))
@@ -18,22 +17,28 @@ ratings = np.random.randint(1,5,size=(len(user_ID),len(title)))
 
 course_ratings = pd.DataFrame(ratings,columns=title,index=user_ID)
 
-def get_sim_courses(title,ratings):
-    sim_score = course_sim_df[title]*(ratings-2.5)
-    sim_score = sim_score.sort(reverse=True)
+def standardize(row):
+    new_row = (row - row.mean()) /(row.max()-row.min())
+    return new_row
 
+def get_sim_course(title,ratings):
+    sim_score = pd.DataFrame(course_sim_df[title]*(ratings-2.5))
+    
     return sim_score
 
-course_sim_df = course_ratings.corr(method='pearson')
+course_ratings_std = course_ratings.apply(standardize)
+course_sim = cosine_similarity(course_ratings_std)
 
-test_user = [("Stormwater Management and Design - An Introduction",5),("Gravity Retaining Walls - Online",1),("First home buyers workshop",4)]
+course_sim_df =pd.DataFrame(course_sim,index=title,columns=title)
+#print(get_sim_course("A day in the life of a Water Engineer",5))
 
-similar_CPD_events = {}
-for title,rating in test_user:
-    similar_CPD_events = get_sim_courses(title,rating)
+test_user = [("A day in the life of a Water Engineer",5),("Waterproofing performance of deep basements",1),("An Introduction to Health and Safety by Design",4)]
 
-similar_CPD_events
-similar_CPD_events.sum()
+similar_CPD_events = pd. DataFrame()
+for course,ratings in test_user:
+     similar_CPD_events = similar_CPD_events.append(get_sim_course(course,ratings))
+
+# similar_CPD_events.sum()
 print(similar_CPD_events)
 
 
